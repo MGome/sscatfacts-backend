af5ba037b119239c5ea96572f529cc0090725798
Mario Gomez <migomez6@uc.cl>
2022-12-26 04:58:48 -0300

    Add model references for likes, users and cat facts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_26_022308) do
+ActiveRecord::Schema.define(version: 2022_12_26_075250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,10 @@ ActiveRecord::Schema.define(version: 2022_12_26_022308) do
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "cat_fact_id"
+    t.bigint "user_id"
+    t.index ["cat_fact_id"], name: "index_likes_on_cat_fact_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +36,6 @@ ActiveRecord::Schema.define(version: 2022_12_26_022308) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "likes", "cat_facts"
+  add_foreign_key "likes", "users"
 end
