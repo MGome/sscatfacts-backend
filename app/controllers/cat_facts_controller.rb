14813fa11fc2b39a0f90a6310464029df1e4b666
Mario Gomez <migomez6@uc.cl>
2022-12-26 05:49:16 -0300

    Add get popular cat facts endpoint

--- app/controllers/cat_facts_controller.rb
+++ app/controllers/cat_facts_controller.rb
@@ -11,4 +11,9 @@ class CatFactsController < ApplicationController
     liked_facts = user.cat_facts.select('id', 'description')
     render json: { likes: liked_facts }
   end
+
+  def popular_cat_facts
+    most_liked = CatFact.left_joins(:likes).select('id', 'description').group(:id).order('COUNT(likes.id) DESC').limit(10)
+    render json: most_liked
+  end
 end
