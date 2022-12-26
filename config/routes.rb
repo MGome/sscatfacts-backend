da6015045765ecb35575d2263f45914c2d41dd9e
Mario Gomez <migomez6@uc.cl>
2022-12-26 05:28:28 -0300

    Add cat facts get cat fact route and controller action

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/cat_fact', to: 'cat_facts#cat_fact'
 end
