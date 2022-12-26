a681242f1505f96ffa6bfea7383655e531303988
Mario Gomez <migomez6@uc.cl>
2022-12-26 05:38:34 -0300

    Add get liked cat facts endpoint

--- app/controllers/cat_facts_controller.rb
+++ app/controllers/cat_facts_controller.rb
@@ -5,4 +5,10 @@ class CatFactsController < ApplicationController
     response = HTTParty.get('https://catfact.ninja/fact')
     render json: response
   end
+
+  def liked_cat_facts
+    user = User.find(params[:id])
+    liked_facts = user.cat_facts.select('id', 'description')
+    render json: { likes: liked_facts }
+  end
 end
