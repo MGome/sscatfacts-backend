8a7fa898eb5790e539a538bdba61b5a7a0893c55
Mario Gomez <migomez6@uc.cl>
2022-12-28 02:13:15 -0300

    Add status codes to cat facts endpoints responses

--- app/controllers/cat_facts_controller.rb
+++ app/controllers/cat_facts_controller.rb
@@ -6,18 +6,18 @@ class CatFactsController < ApplicationController
     fact = CatFact.find_or_create_by(description: response['fact'])
     response['fact_id'] = fact.id
     response['liked_fact'] = User.find(params[:user_id]).cat_facts.where(id: fact.id).present?
-    render json: { body: response }
+    render json: { body: response }, status: 200
   end
 
   def liked_cat_facts
     user = User.find(params[:id])
     liked_facts = user.cat_facts.select('id', 'description')
-    render json: { body: liked_facts }
+    render json: { body: liked_facts }, status: 200
   end
 
   def popular_cat_facts
     most_liked = CatFact.left_joins(:likes).select('id', 'description').group(:id).order('COUNT(likes.id) DESC').limit(10)
-    render json: { body: most_liked }
+    render json: { body: most_liked }, status: 200
   end
 
   def like_cat_fact
@@ -25,9 +25,9 @@ class CatFactsController < ApplicationController
     cat_fact = CatFact.find(params[:fact_id])
     new_like = Like.new(user_id: user.id, cat_fact_id: cat_fact.id)
     if new_like.save
-      render json: { body: new_like }
+      render json: { body: new_like }, status: 201
     else
-      render json: { body: { errors: new_like.errors.full_messages } }
+      render json: { body: { errors: new_like.errors.full_messages } }, status: 400
     end
   end
 end
