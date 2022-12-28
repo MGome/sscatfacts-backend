97a7136ae4ca32418e3996f06e4a427c91513e46
Mario Gomez <migomez6@uc.cl>
2022-12-28 13:18:45 -0300

    Fix users requests test file syntax errors

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,10 +2,10 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do # rubocop:disable Metrics/BlockLength
+RSpec.describe 'Users', type: :request do
   context 'when request is POST users#create with correct params' do
     it 'should increment users by one and respond created status code' do
-      expect { post create_user_path, params: { username: "sample-name" } }.to change(User, :count).by(+1)
+      expect { post create_user_path, params: { username: 'sample-name' } }.to change(User, :count).by(+1)
       expect(response).to have_http_status(:created)
     end
   end
