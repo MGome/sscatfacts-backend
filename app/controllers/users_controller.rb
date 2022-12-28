0421ed23c40ffc5d679e4264336ab11884aae5c2
Mario Gomez <migomez6@uc.cl>
2022-12-28 02:10:37 -0300

    Add status codes to user endpoints responses

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,18 @@ class UsersController < ApplicationController
   def create
     @user = User.new(username: params[:username])
     if @user.save
-      render json: { body: ActiveSupport::JSON.encode(@user) }
+      render json: { body: ActiveSupport::JSON.encode(@user) }, status: 201
     else
-      render json: { body: { errors: @user.errors.full_messages } }
+      render json: { body: { errors: @user.errors.full_messages } }, status: 400
     end
   end
 
   def find_user
     @user = User.where(username: params[:username])
     if @user.present?
-      render json: { body: ActiveSupport::JSON.encode(@user.first) }
+      render json: { body: ActiveSupport::JSON.encode(@user.first) }, status: 200
     else
-      render json: { errors: 'No existe un usuario con este username' }
+      render json: { errors: 'No existe un usuario con este username' }, status: 404
     end
   end
 end
