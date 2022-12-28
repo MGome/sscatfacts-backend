a60eddae585e3f1161015388b70427f9b8d4f664
Mario Gomez <migomez6@uc.cl>
2022-12-28 05:36:01 -0300

    Add cat facts requests tests cases with less params

--- spec/requests/cat_facts_spec.rb
+++ spec/requests/cat_facts_spec.rb
@@ -3,10 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe 'CatFacts', type: :request do # rubocop:disable Metrics/BlockLength
-  context 'when request is GET cat_facts#cat_fact' do
+  context 'when request is GET cat_facts#cat_fact with user_id' do
+    let!(:new_user) { create(:user) }
+
     it 'should find a cat fact and respond ok status code' do
-      get cat_fact_path
+      get cat_fact_path, params: { user_id: new_user.id }
       expect(response).to have_http_status(:ok)
+    end
+  end
+
+  context 'when request is GET cat_facts#cat_fact without user_id' do
+    it 'should not find a cat fact and respond bad request status code' do
+      get cat_fact_path
+      expect(response).to have_http_status(:bad_request)
     end
   end
 
@@ -17,6 +26,24 @@ RSpec.describe 'CatFacts', type: :request do # rubocop:disable Metrics/BlockLeng
     it 'should increment likes by one and respond created status code' do
       expect { post like_cat_fact_path, params: { user_id: new_user.id, fact_id: new_cat_fact.id } }.to change(Like, :count).by(+1)
       expect(response).to have_http_status(:created)
+    end
+  end
+
+  context 'when request is POST cat_facts#like_cat_fact without user_id' do
+    let!(:new_cat_fact) { create(:cat_fact) }
+
+    it 'should respond bad request status code' do
+      post like_cat_fact_path, params: { fact_id: new_cat_fact.id }
+      expect(response).to have_http_status(:bad_request)
+    end
+  end
+
+  context 'when request is POST cat_facts#like_cat_fact without fact_id' do
+    let!(:new_user) { create(:user) }
+
+    it 'should respond bad request status code' do
+      post like_cat_fact_path, params: { user_id: new_user.id }
+      expect(response).to have_http_status(:bad_request)
     end
   end
 
@@ -40,20 +67,4 @@ RSpec.describe 'CatFacts', type: :request do # rubocop:disable Metrics/BlockLeng
       expect(response).to have_http_status(:ok)
     end
   end
-
-  # context 'when request is GET users#find_user with correct params' do
-  #   let!(:new_user) { create(:user) }
-
-  #   it 'should find the user and respond ok status code' do
-  #     get find_user_path, params: { username: new_user.username }
-  #     expect(response).to have_http_status(:ok)
-  #   end
-  # end
-
-  # context 'when request is GET users#find_user with incorrect params' do
-  #   it 'should not find the user and respond not found status code' do
-  #     get find_user_path
-  #     expect(response).to have_http_status(:not_found)
-  #   end
-  # end
 end
